def1d73adb135a18ce6e4a59000e64dbd6546765
Leesoo Ahn <lsahn@ooseel.net>
2021-01-20 21:10:43 +0900

    applets/cat: add basic prototype

--- applets/cat.go
+++ applets/cat.go
@@ -1,11 +1,32 @@
 package applets
 
-import (
-	"fmt"
-)
+import "os"
 
+// CatMain returns 0 on success
 func CatMain(args []string) int {
-	fmt.Println("Hello at cat main")
+	const BUFSZ = 1024 * 4
+	rdbuf := make([]byte, BUFSZ)
+
+	for _, fname := range args {
+		fp, err := os.Open(fname)
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			n, err := fp.Read(rdbuf)
+			if err != nil {
+				panic(err)
+			}
+			os.Stdout.Write(rdbuf[:n])
+
+			if n < BUFSZ {
+				break
+			}
+		}
+
+		fp.Close()
+	}
 
 	return 0
 }
