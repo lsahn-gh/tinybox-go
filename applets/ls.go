3311cda6722624291b4193a3843d0b6b3d247e49
Leesoo Ahn <lsahn@ooseel.net>
2021-01-20 21:32:38 +0900

    add 'ls' cmd prototype

---
+++ applets/ls.go
@@ -0,0 +1,7 @@
+package applets
+
+func LsMain(args []string) int {
+	println("Hello at ls main")
+
+	return 0
+}
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type funcSym func(args []string) int
 // TODO: how to kill the warn?
 var appletTbl map[string]funcSym = map[string]funcSym{
 	"cat": applets.CatMain,
+	"ls":  applets.LsMain,
 }
 
 func main() {
@@ -33,6 +34,6 @@ func panicException() {
 		fmt.Print(
 			"usage: tinybox-go <applets>\n\n",
 			"- applets\n",
-			"    cat\n")
+			"    cat ls\n")
 	}
 }
